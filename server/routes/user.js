6c0fc870998458c78ff270b58b3070d881e6e175
v_liangqin <v_liangqin@tencent.com>
2022-09-05 14:16:44 +0800

    环境监测

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,6 +6,8 @@ const router = Router()
 router.prefix('/user')
 
 router.get('/getUserInfo', async (ctx, next) => {
+  console.log('所在的环境')
+  console.log(process.env.NODE_ENV)
   const uid = ctx.query.uid
   const user = await query(`select * from account where uid = ?`, [uid])
   ctx.body = user[0]
